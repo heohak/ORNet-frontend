1d34057cb324e630a6186d2fa080cd515ed61a17
Samuel <saarge@ttu.ee>
2024-07-02 16:10:06 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and hello world.
         </p>
         <a
           className="App-link"
