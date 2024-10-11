7c5b7e871383326192a700e1f932be85aa8f3857
qidoqga <leplandjoosep28@gmail.com>
2024-10-11 14:54:25 +0300

    config right ip

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
-    // API_BASE_URL: 'http://192.168.1.49:8080',
-    API_BASE_URL: 'http://localhost:8080',
+    API_BASE_URL: 'http://192.168.1.49:8080',
+    // API_BASE_URL: 'http://localhost:8080',
 };
 
 export default config;
