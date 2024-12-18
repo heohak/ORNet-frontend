3a116ab1bed45d7905e8fdca4a68016c68460123
qidoqga <leplandjoosep28@gmail.com>
2024-12-18 16:42:56 +0200

    ticket delete is only available for admins

--- src/pages/TicketsPage/SingleTicketModal/TicketDeleteModal.js
+++ src/pages/TicketsPage/SingleTicketModal/TicketDeleteModal.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import {Modal, Button, Alert} from "react-bootstrap";
 
-const TicketDeleteModal = ({ show, handleClose, handleDelete }) => {
+const TicketDeleteModal = ({ show, handleClose, handleDelete, error }) => {
     return (
         <Modal show={show} onHide={handleClose} centered>
             <Modal.Header closeButton>
                 <Modal.Title>Confirm Deletion</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                {error && (
+                    <Alert variant="danger" className="mt-3">
+                        {error}
+                    </Alert>
+                )}
                 <p>Are you sure you want to delete this ticket? This change is permanent.</p>
             </Modal.Body>
             <Modal.Footer>
