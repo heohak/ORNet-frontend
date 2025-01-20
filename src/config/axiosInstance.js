ff65fae0e6f95d5f5bc439e8e7c23a45d4ede691
heohak <heohak@ttu.ee>
2025-01-20 15:58:25 +0200

    device extra field value is not required when creating new field

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "http://localhost:8080/api",
-    baseURL: "http://192.168.1.49:8080/api",
+     baseURL: "http://localhost:8080/api",
+    //baseURL: "http://192.168.1.49:8080/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
